ef4ade82ca40aa924b57200969903b29cdef4d70
mundfanemahesh <100686790+mundfanemahesh@users.noreply.github.com>
2022-03-13 20:27:13 +0530

    main added

--- Demo2/src/demo2package/Democlass2.java
+++ Demo2/src/demo2package/Democlass2.java
@@ -5,6 +5,12 @@ public class Democlass2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("hello");
+			main();
 	}
+	public static void main() {
+		// TODO Auto-generated method stub
+			System.out.println("hello");
+	}
+
 
 }
